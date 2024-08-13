29f575c8b2a471795a2f776e8bef82b680337e79
Abdulrazzaq Alhendi <dev@aalhendi.com>
2024-08-13 15:02:30 +0300

    feat(db): add subscription tokens table and backfill subscription status and make mandatory

---
+++ migrations/20240813084610_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,18 @@
+-- Wrap whole migration in a transaction to make sure it succeeds or fails atomically. 
+-- `sqlx` does not do this automatically.
+BEGIN;
+
+-- Backfill `status` for historical entries
+UPDATE subscriptions
+SET
+    status = 'confirmed'
+WHERE
+    status IS NULL;
+
+-- Make `status` mandatory
+ALTER TABLE subscriptions
+ALTER COLUMN status
+SET
+    NOT NULL;
+
+COMMIT;
