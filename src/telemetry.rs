e520a27a8fd256dac92ae1ddb7d68b9234ff62cc
Abdulrazaq Alhendi <dev@aalhendi.com>
2024-08-24 20:25:49 +0300

    refactor: avoid blocking async executor

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,3 +1,4 @@
+use tokio::task::JoinHandle;
 use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
@@ -33,4 +34,15 @@ pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     // Redirect all `log`'s events to our subscriber
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber"); // specify default subscriber to process tacing spans
+}
+
+/// Inherits Singature from tokio::task::spawn_blocking
+pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
+where
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static,
+{
+    let current_span = tracing::Span::current();
+    // Explicitly attach current span to newly spawned thread to maintain span context.
+    tokio::task::spawn_blocking(move || current_span.in_scope(f))
 }
