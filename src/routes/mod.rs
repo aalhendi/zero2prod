3dbd9fa97edf4f2e6bb17e2edb487cd0309f5db8
Abdulrazaq Alhendi <dev@aalhendi.com>
2024-12-27 13:04:00 +0300

    chore: cargo fmt and sqlx prepare

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,7 @@
 pub mod admin;
+pub mod forgot_password;
 pub mod health_check;
 pub mod home;
 pub mod login;
 pub mod subscriptions;
 pub mod subscriptions_confirm;
-pub mod forgot_password;
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -4,10 +4,10 @@
 
 mod admin_dashboard;
 mod change_password;
+mod forgot_password;
 mod health_check;
 mod helpers;
 mod login;
 mod newsletter;
 mod subscriptions;
 mod subscriptions_confirm;
-mod forgot_password;
